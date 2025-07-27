f409dc701ac94d92ff2371ce0c89557a0262cb41
Daniel Grychtoł <grychtol.daniel@gmail.com>
2025-07-27 15:25:41 +0200

    feat: edit example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,33 +1,56 @@
 import { useState } from 'react';
-import { Text, View, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  useWindowDimensions,
+  ScrollView,
+  type ViewStyle,
+} from 'react-native';
 import { scanDocuments } from 'react-native-document-camera';
+import type { DocumentScan } from '../../src/types/DocumentScanner';
 
 export default function App() {
-  const [result, setResult] = useState<string>();
-  const [ocr, setOcr] = useState<string>();
+  const { width, height } = useWindowDimensions();
+  const [scans, setScans] = useState<DocumentScan[]>([]);
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity
-        onPress={async () => {
-          const uris = await scanDocuments().catch(console.log);
+      <View style={styles.scanFab}>
+        <TouchableOpacity
+          style={styles.fab}
+          onPress={async () => {
+            try {
+              const scansResponse = await scanDocuments();
 
-          setResult(uris?.[0]?.imageUri ?? '');
-          setOcr(uris?.[0]?.ocrText ?? 'no ocr');
-        }}
-      >
-        <Text>Scan</Text>
-      </TouchableOpacity>
-      <Text style={{ marginBottom: 20 }}>Result: {JSON.stringify(result)}</Text>
-      <Text>Result: {JSON.stringify(ocr)}</Text>
-      {result && (
-        <Image
-          src={result || ''}
-          width={299}
-          height={299}
-          resizeMode="contain"
-        />
-      )}
+              setScans(scansResponse);
+            } catch (_) {
+              //
+            }
+          }}
+        >
+          <Text>Scan</Text>
+        </TouchableOpacity>
+      </View>
+
+      <ScrollView snapToInterval={width} horizontal>
+        {scans.map((scan, index) => (
+          <View style={[$slide(width, height)]} key={String(index)}>
+            <Text style={styles.spacedText}>OCR:</Text>
+            <Text>{scan?.ocrText ?? 'NO OCR'}</Text>
+            {scan.imageUri && (
+              <Image
+                src={scan.imageUri || ''}
+                width={width}
+                style={styles.image}
+                resizeMode="contain"
+              />
+            )}
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -39,4 +62,26 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: 'green',
   },
+  scanFab: {
+    position: 'absolute',
+    bottom: 40,
+    right: 40,
+  },
+  fab: {
+    width: 40,
+    height: 40,
+    backgroundColor: 'blue',
+  },
+  spacedText: {
+    marginBottom: 20,
+  },
+  image: {
+    aspectRatio: 1,
+  },
 });
+
+const $slide = (width = 100, height = 100) =>
+  ({
+    width,
+    height,
+  }) satisfies ViewStyle;
